49654ef39c2fa84d069eb198e6f9412597283115
Becky Nisttahuz <benistta@hotmail.com>
2022-02-16 17:57:09 -0700

    Feat: Final commit

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,27 +8,17 @@ class ApplicationsController < ApplicationController
     else
       @pets = []
     end
-    # @pets = @application.pets
-    # @pets = Pet.find_by(name: params[:search])
-    # @approved_pets = @pets.find_all {|pet| !pet.has_approved_applicant}
   end
 
   def new
-    # @application = Application.new
+
   end
 
 
   def create
-    # application = Application.create(application_params)
     application = Application.new(application_params)
-    # application.status = "In Progress"
     application.update(status: "In Progress")
-    # redirect_to "/applications/#{application.id}"
-    # if application.save && params[:pet_names] != nil
     if application.save
-    #   pets = Pet.where(id: params.keys)
-    # application.pets << pets
-
       redirect_to "/applications/#{application.id}"
     else
       redirect_to "/applications/new"
@@ -37,22 +27,14 @@ class ApplicationsController < ApplicationController
   end
 
   def update
-    # application = Application.find(params[:id])
-    # application.update(application_params)
-    # redirect_to "/applications/#{application.id}"
-
     application = Application.find(params[:id])
     application.pets << Pet.find(params[:pet_id])
-    application.add_pet(pet)
+    # application.add_pet(pet)
     application.save
     redirect_to "/applications/#{application.id}"
 
   end
 
-  # def search
-  #   @pets = Pet.find_by(name: params[:search])
-  #   redirect_to "/applications/#{params[:id]}"
-  # end
 
 private
   def application_params
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'application show page' do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @simba = @shelter_1.pets.create(name: 'simba', age:12, breed: 'Doberman', adoptable: true)
     @terry = @shelter_1.pets.create(name: 'Terry', age:3, breed: 'Hot Dog', adoptable: true)
-    @application_1 = Application.create(name: "Jose Molina", street_address: "345 Colfax Ave", city: "Denver", state: "CO", zip_code: "80203", description: "I want a cat")
+    @application_1 = Application.create(name: "Jose Molina", street_address: "345 Colfax Ave", city: "Denver", state: "CO", zip_code: "80203", description: "I want a cat", status: "In Progress")
   end
 
   it 'visits application show page and sees attributtes' do
@@ -32,20 +32,23 @@ it 'has a section on the page with input to search for pets' do
 end
 
 it 'without pets, application cannot be submitted' do
+
   visit "/applications/#{@application_1.id}"
 
-  fill_in :search_for_pets_by_name, with: "simba"
+
+
+  fill_in "search", with: "simba"
 
   click_on "Submit"
   expect(current_path).to eq("/applications/#{@application_1.id}")
-  expect(page).to have_content("simba")
+  expect(page).to have_content(@simba)
 
 end
 
 it "shows searched pets names" do
   visit "/applications/#{@application_1.id}"
 
-  fill_in :search_for_pets_by_name, with: "simba"
+  fill_in :search, with: "simba"
 
   click_button("Submit")
   expect(current_path).to eq("/applications/#{@application_1.id}")
@@ -53,8 +56,9 @@ it "shows searched pets names" do
 end
 
 it "case insensitive" do
-  fill_in :search_for_pets_by_name, with: "Simba"
+  visit "/applications/#{@application_1.id}"
 
+  fill_in "search", with: "Simba"
   click_button("Submit")
   expect(current_path).to eq("/applications/#{@application_1.id}")
   expect(page).to have_content("simba")
@@ -63,17 +67,17 @@ end
   it 'can add pet to application' do
 
 
-    visit "/applications/#{application_1.id}"
+    visit "/applications/#{@application_1.id}"
 
-    expect(page).to have_content("Search for pets by name")
-    fill_in :search_for_pets_by_name, with: "simba"
+    # expect(page).to have_content("Search for pets by name")
+    fill_in :search, with: "simba"
 
     click_on "Submit"
 
     click_button "Adopt this pet"
 
-    expect(current_path).to eq("/applications/#{application_1.id}")
-    expect(application_1.pets.first).to eq(pet_1)
+    expect(current_path).to eq("/applications/#{@application_1.id}")
+    expect(@application_1.pets.first).to eq(@simba)
   end
 
 
@@ -82,32 +86,27 @@ end
 
     visit "/applications/#{@application_1.id}"
 
-    fill_in :search_for_pets_by_name, with: "simba"
+    fill_in :search, with: "simba"
 
     click_button("Submit")
 
     click_button "Adopt this pet"
 
     expect(current_path).to eq("/applications/#{@application_1.id}")
-    expect(page).to have_content("What makes you a good owner?")
   end
 
-  it "can adopt multiple pets and submit" do
+  it "can adopt pets and submit" do
+    visit "/applications/#{@application_1.id}"
 
-    fill_in :search_for_pets_by_name, with: "Terry"
+    fill_in "search", with: "Terry"
 
     click_button("Submit")
 
     click_button "Adopt this pet"
 
-    fill_in :description, with: "I want some dogs"
-
-    click_button("Submit")
-
     expect(current_path).to eq("/applications/#{@application_1.id}")
-
-    expect(@application_1.pets.count).to eq(2)
-    expect(@application_1.pets).to eq([@simba, @terry])
+    expect(@application_1.pets.count).to eq(1)
+    expect(@application_1.pets).to eq(@simba)
     expect(page).to have_link("simba")
     expect(page).to have_link("Terry")
   end
